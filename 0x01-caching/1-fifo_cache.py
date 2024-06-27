fc1adcf50f2464247d2283a31d362cec657f5268
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-06-27 05:58:51 +0100

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -11,14 +11,14 @@ class FIFOCache(BaseCaching):
 
     def __init__(self):
         """
-        Initialize the class with the parent's init method
+        a funx that Initializes the class with the parent's init method
         """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
         """
-        Cache a key-value pair
+        this func Caches a key-value pair
         """
         if key is None or item is None:
             pass
@@ -33,7 +33,7 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """
-        Return the value linked to a given key, or None
+        it Returns the value linked to a given key, or None
         """
         if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
